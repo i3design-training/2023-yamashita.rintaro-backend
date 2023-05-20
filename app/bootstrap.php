4266a2a625efc0e7132cf6c81d75c3cb0263cf44
KUBO Seiya <s.kubo@i3design.co.jp>
2023-05-20 15:54:25 +0900

    環境変数取得

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,8 +4,12 @@ declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
 
+use Dotenv\Dotenv;
 use Illuminate\Database\Capsule\Manager;
 use Slim\Factory\AppFactory;
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->safeLoad();
 
 $app = AppFactory::create();
 
@@ -13,7 +17,15 @@ $routes = require __DIR__ . '/routes.php';
 $routes($app);
 
 $manager = new Manager();
-$manager->addConnection([]);
+$manager->addConnection([
+    'driver' => 'pgsql',
+    'host' => getenv('DB_HOST') ?: 'localhost',
+    'port' => getenv('DB_PORT') ?: 5432,
+    'database' => getenv('DB_DATABASE') ?: 'todo',
+    'username' => getenv('DB_USERNAME') ?: 'todo',
+    'password' => getenv('DB_PASSWORD') ?: 'todo',
+    'prefix' => '',
+]);
 $manager->bootEloquent();
 
 return $app;
