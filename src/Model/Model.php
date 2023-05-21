4c0328970801dcc6b29d1f317f0e1876ce6ca64f
KUBO Seiya <s.kubo@i3design.co.jp>
2023-05-21 19:51:02 +0900

    use eloquent native uuid

--- src/Model/Model.php
+++ src/Model/Model.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 namespace App\Model;
 
-use App\Model\Constraint\Uuid;
 use Illuminate\Database\Eloquent\Model as BaseModel;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
 abstract class Model extends BaseModel
 {
-    use Uuid;
+    use HasUuids;
 }
